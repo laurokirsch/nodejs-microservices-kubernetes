cde472e1dbab1da4126a68829d8a12a78dd32937
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-06-12 18:08:48 -0300

    create user model and persist to db

---
+++ auth/src/models/user.ts
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+// props required to create a new user
+interface UserAttrs {
+  email: String;
+  password: String;
+}
+
+// describes the `User` model
+// extends the base model and adds the static method `build` to make use of the `UserAttrs`
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  email: String;
+  password: String;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+// `UserAttrs` interface serves the purpose of type-checking without being an `User` type
+// this adds static props to the `User` model
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+
+// `User` type usually has a few other fields e.g. `createdAt`, etc
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from './../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = Router();
 
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -23,12 +23,17 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-    throw new Error();
+    if (existingUser) {
+      console.log('Email in use');
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
