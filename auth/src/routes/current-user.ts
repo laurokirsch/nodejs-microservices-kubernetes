742e4a56a9008967e8e7215ef0b0e7e7db1f4c49
Lauro Kirsch <lauro.kirsch@gmail.com>
2020-06-13 16:23:29 -0300

    create signout handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('current user route');
+  if (!req.session?.jwt) return res.send({ currentUser: null });
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import { Router } from 'express';
 const router = Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('signout route');
+  req.session = null;
+  res.send({});
 });
 
 export { router as signoutRouter };
